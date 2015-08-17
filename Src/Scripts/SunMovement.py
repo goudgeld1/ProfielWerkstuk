14fb24b1648fc316ddab2dcd8799ac56bb031f32
goudgeld1 <thom137@hotmail.com>
2015-08-17 14:38:01 +0200

    Masive Update
    + A lot of 3d models
    ^ Terrain and terrain textures
    ^ Sun and shadows
    ^ A lot small tweakes

--- Src/Scripts/SunMovement.py
+++ Src/Scripts/SunMovement.py
@@ -7,6 +7,7 @@ scenes = bge.logic.getSceneList()
 
 # Get the Objects
 sunPath = curScene.objects["SunPath"]
+sun = curScene.objects["Sun"]
 ambient = curScene.objects["Ambient"]
 
 xyz = sunPath.localOrientation.to_euler()
@@ -16,14 +17,17 @@ sunPath.localOrientation = xyz.to_matrix()
 # make the sky color change
 if(math.degrees(xyz[1]) < 80 and math.degrees(xyz[1]) > -80):
     ambient.energy = 0.4
+    sun.energy = 1.0
     ambient.color = [1, 1, 1]
     bge.render.setBackgroundColor([0, .4, .65, 0])
 elif(math.degrees(xyz[1]) > 100 or math.degrees(xyz[1]) < -100):
     ambient.energy = 0.2
+    sun.energy = 0.0
     ambient.color = [.4, .4, .8]
     bge.render.setBackgroundColor([0, 0, .1, 0])
 else:
     ambient.energy = 0.3
+    sun.energy = 0.3
     ambient.color = [.6, .15, .05]
     bge.render.setBackgroundColor([.4, .3, .6, 0])
     
--- Src/Scripts/init.py
+++ Src/Scripts/init.py
@@ -1,3 +1,16 @@
 import bge
 
-print("lol")
+# Frequently used variables
+curScene = bge.logic.getCurrentScene()
+curCont = bge.logic.getCurrentController()
+scenes = bge.logic.getSceneList()
+
+# Get the objects
+fpView = curScene.objects["FpView"]
+player = curScene.objects["Player"]
+
+# Add the HeadsUpDisplay (HUD) to the view
+if player["HUD"]:
+    Scene = player.actuators["HUD"]
+    Scene.scene = "HUD"
+    curCont.activate("HUD")
